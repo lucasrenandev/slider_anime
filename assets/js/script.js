cf0f2cd43385dc309f3335e99fa90dd59ed5143e
lucasrenandns <lucasrenandns96@gmail.com>
2022-12-22 13:16:34 -0300

    Slider

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+const previousButton = document.querySelector(".previous-button")
+const nextButton = document.querySelector(".next-button")
+const slideImage = document.querySelectorAll(".slide-image")
+const randomButtonContainer = document.querySelector(".random-button-container")
+
+let currentSlide = 0
+
+function hideSlide() {
+    slideImage.forEach(image => image.classList.remove("active"))
+}
+
+function showSlide() {
+    slideImage[currentSlide].classList.add("active")
+}
+
+function nextSlide() {
+    if(currentSlide < slideImage.length - 1) {
+        currentSlide ++
+    }
+    else {
+        currentSlide = 0
+    }
+    hideSlide()
+    showSlide()
+}
+
+function previousSlide() {
+    if(currentSlide === 0) {
+        currentSlide = slideImage.length - 1
+    }
+    else {
+        currentSlide--
+    }
+    hideSlide()
+    showSlide()
+}
+
+function randomSlide() {
+   currentSlide = Math.floor(Math.random() * slideImage.length)
+   hideSlide()
+   showSlide()
+}
+
+nextButton.addEventListener("click", nextSlide)
+previousButton.addEventListener("click", previousSlide)
+randomButtonContainer.addEventListener("click", randomSlide)
